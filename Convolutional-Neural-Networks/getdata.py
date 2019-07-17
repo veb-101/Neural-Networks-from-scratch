01baadad34a226e0f895d5cf0df40c26857ec90b
Vaibhav Singh <vaibhav.singh.3001@gmail.com>
2019-07-18 02:18:51 +0530

    Added file for preparing data

---
+++ Convolutional-Neural-Networks/getdata.py
@@ -0,0 +1,30 @@
+import mnist
+
+
+def reshapeImages(images):
+    '''
+    reshaping the dataset into shape (m, h*w) for images
+    and (m, 1) for labels
+    '''
+    images = images.reshape(images.shape[0], -1)
+    return images
+
+
+def reshapedMnistData(train_images, train_labels, test_images, test_labels):
+    train_images = reshapeImages(train_images)
+    train_labels = reshapeImages(train_labels)
+    test_images = reshapeImages(test_images)
+    test_labels = reshapeImages(test_labels)
+    return train_images, train_labels, test_images, test_labels
+
+
+def getMnistData(reshaped=True):
+    mnist.temporary_dir = lambda: r'.\dataset'
+    train_images = mnist.train_images()
+    train_labels = mnist.train_labels()
+    test_images = mnist.test_images()
+    test_labels = mnist.test_labels()
+    if reshaped == True:
+        return reshapedMnistData(train_images, train_labels, test_images, test_labels)
+    else:
+        return train_images, train_labels, test_images, test_labels
