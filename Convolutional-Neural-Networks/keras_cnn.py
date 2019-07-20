06d024b40522c17132e43ed74b9aec64591bc9c7
Vaibhav Singh <vaibhav.singh.3001@gmail.com>
2019-07-20 21:50:30 +0530

    Added cnn using keras

---
+++ Convolutional-Neural-Networks/keras_cnn.py
@@ -0,0 +1,60 @@
+import keras
+from keras.datasets import mnist
+from keras.models import Sequential
+from keras.layers import Dense, Dropout, Flatten
+from keras.layers import Conv2D, MaxPooling2D
+from keras.utils import to_categorical
+from keras.losses import categorical_crossentropy
+from keras.optimizers import Adadelta
+from keras import backend as K
+
+batch_size = 128
+num_classes = 10
+epochs = 12
+
+img_rows, img_cols = 28, 28
+
+(x_train, y_train), (x_test, y_test) = mnist.load_data()
+
+if K.image_data_format() == 'channels_first':
+    x_train = x_train.reshape(x_train.shape[0], 1, img_rows, img_cols)
+    x_test = x_test.reshape(x_test.shape[0], 1, img_rows, img_cols)
+    input_shape = (1, img_rows, img_cols)
+else:
+    x_train = x_train.reshape(x_train.shape[0], img_rows, img_cols, 1)
+    x_test = x_test.reshape(x_test.shape[0], img_rows, img_cols, 1)
+    input_shape = (img_rows, img_cols, 1)
+
+x_train = x_train.astype('float32')
+x_test = x_test.astype('float32')
+x_train /= 255
+x_test /= 255
+
+print(f'x_train shape: {x_train.shape}')
+print(f'{x_train.shape[0]} train samples')
+print(f'{x_test.shape[0]} test samples')
+
+y_train = to_categorical(y_train, num_classes)
+y_test = to_categorical(y_test, num_classes)
+
+model = Sequential()
+model.add(Conv2D(32, kernel_size=(3, 3), activation='relu',
+                 input_shape=input_shape))
+model.add(Conv2D(64, kernel_size=(3, 3), activation='relu'))
+model.add(MaxPooling2D(pool_size=(2, 2)))
+model.add(Dropout(0.25))
+model.add(Flatten())
+model.add(Dense(128, activation='relu'))
+model.add(Dropout(0.5))
+model.add(Dense(num_classes, activation='softmax'))
+
+model.compile(loss=categorical_crossentropy, optimizer=Adadelta(),
+              metrics=['accuracy'])
+
+model.fit(x_train, y_train, batch_size=batch_size, epochs=epochs, verbose=1,
+          validation_split=0.2)
+
+score = model.evaluate(x_test, y_test)
+
+print(f'Test loss: {score[0]}')
+print(f'Test accuracy: {score[1]}')
