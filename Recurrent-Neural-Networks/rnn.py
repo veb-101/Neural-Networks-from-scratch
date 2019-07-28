45c2b543f6b0758d3f24b87e8c3227b234002247
Vaibhav Singh <vaibhav.singh.3001@gmail.com>
2019-07-28 14:17:08 +0530

    BPTT implemented

--- Recurrent-Neural-Networks/rnn.py
+++ Recurrent-Neural-Networks/rnn.py
@@ -33,17 +33,53 @@ class RNN:
             self.last_hs[i+1] = h
 
         y = self.Why @ h + self.by
-
+        print(h.shape)
         return y, h
 
-    def backprop(self, d_y, learning_rate=2e-2):
+    def backprop(self, dL_dy, learning_rate=2e-2):
         '''
         Perform a backward pass of the RNN
-        - d_y (dL/dy) has shape (output_size, 1)
+        - dL_dy (dL/dy) has shape (output_size, 1)
         - learning_rate is a float
         '''
 
-        # n = len(self.last_inputs)
-        # d_Why = d_y @ self.last_hs[n].T
-        # d_by = d_y
-        pass
+        n = len(self.last_inputs)
+        d_Why = dL_dy @ self.last_hs[n].T
+        d_by = dL_dy
+
+        # Initialize dL/dWhh, dL/dWxh, dL/dbh to zeros
+
+        d_Whh = np.zeros(self.Whh.shape)
+        d_Wxh = np.zeros(self.Wxh.shape)
+        d_bh = np.zeros(self.bh.shape)
+
+        # dL/dh for the last h
+        dh = self.Why.T @ dL_dy
+
+        # Backpropagate through time
+        for t in reversed(range(n)):
+            # intermediate value: dL/dh * (1 - h^2)
+            temp = (1 - self.last_hs[t+1] ** 2) * dh
+
+            # dL/db = dL/dh * (1 - h^2) = temp
+            d_bh += temp
+
+            # dL/d_Whh = dL/dh * (1 - h^2) * h_{t-1} = temp * currect ht
+            d_Whh += temp @ self.last_hs[t].T
+
+            # dL/Wxh = dL/dh * (1 - h^2) * x = temp * current input
+            d_Wxh += temp @ self.last_inputs[t].T
+
+            # dL/dh = dL/dh * (1 - h^2) * Whh = temp * Whh
+            dh = self.Whh @ temp
+
+            # Clip to prevent exploding gradients
+            for d in [d_Wxh, d_Whh, d_Why, d_bh, d_by]:
+                np.clip(d, -1, 1, out=d)
+
+            # updates
+            self.Wxh -= learning_rate * d_Wxh
+            self.Whh -= learning_rate * d_Whh
+            self.Why -= learning_rate * d_Why
+            self.bh -= learning_rate * d_bh
+            self.by -= learning_rate * d_by
